8098348a83d217b9b2a4ecf1e95065c659f92a8b
judit <a24judsarand@inspedralbes.cat>
2026-04-12 19:55:08 +0200

    BomberMan : personatge amb idles, moviment, mort i Items

--- BomberMan_PROJ3/Assets/Scripts/Game/MovementController.cs
+++ BomberMan_PROJ3/Assets/Scripts/Game/MovementController.cs
@@ -11,10 +11,18 @@ public class MovementController : MonoBehaviour
     public KeyCode inputLeft = KeyCode.A;
     public KeyCode inputRight = KeyCode.D;
 
+    public AnimatedSpriteRenderer spriteRendererUp;
+    public AnimatedSpriteRenderer spriteRendererDown;
+    public AnimatedSpriteRenderer spriteRendererLeft;
+    public AnimatedSpriteRenderer spriteRendererRight;
+    public AnimatedSpriteRenderer spriteRendererDeath;
+    private AnimatedSpriteRenderer activeSpriteRenderer;
+
 
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        activeSpriteRenderer = spriteRendererDown;
     }
     
 
@@ -22,19 +30,19 @@ public class MovementController : MonoBehaviour
     {
         if (Input.GetKey(inputUp))
         {
-            SetDirection(Vector2.up);
+            SetDirection(Vector2.up , spriteRendererUp);
         } else if (Input.GetKey(inputDown))
         {
-            SetDirection(Vector2.down);
+            SetDirection(Vector2.down, spriteRendererDown);
         } else if (Input.GetKey(inputLeft))
         {
-            SetDirection(Vector2.left);
+            SetDirection(Vector2.left , spriteRendererLeft);
         } else if (Input.GetKey(inputRight))
         {
-            SetDirection(Vector2.right);
+            SetDirection(Vector2.right , spriteRendererRight);
         } else
         {
-          SetDirection(Vector2.zero);   
+          SetDirection(Vector2.zero, activeSpriteRenderer);   
         }
 }
 
@@ -47,8 +55,44 @@ private void FixedUpdate()
         rigidbody.MovePosition(position + translation);
 
     }
-private void SetDirection(Vector2 newDirection)
+private void SetDirection(Vector2 newDirection, AnimatedSpriteRenderer spriteRenderer)
     {
         direction = newDirection;
+
+       spriteRendererUp.enabled = spriteRenderer == spriteRendererUp;
+       spriteRendererDown.enabled = spriteRenderer == spriteRendererDown;
+       spriteRendererLeft.enabled = spriteRenderer == spriteRendererLeft;
+       spriteRendererRight.enabled = spriteRenderer == spriteRendererRight;
+
+        activeSpriteRenderer = spriteRenderer;
+        activeSpriteRenderer.idle = direction == Vector2.zero;
     }
-}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Explosion")){
+            DeathSequence();
+        }
+    }
+
+    private void DeathSequence()
+    {
+        enabled = false;
+        GetComponent<BombController>().enabled = false ;
+
+        spriteRendererUp.enabled = false;
+        spriteRendererDown.enabled = false;
+        spriteRendererLeft.enabled = false;
+        spriteRendererRight.enabled = false;
+        spriteRendererDeath.enabled = true;
+
+        Invoke(nameof(OnDeathSequenceEnded), 1.25f);
+
+
+    }
+
+    private void OnDeathSequenceEnded()
+    {
+      gameObject.SetActive(false);
+    }
+} 
